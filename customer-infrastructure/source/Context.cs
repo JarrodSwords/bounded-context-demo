1d107f4af1216ace2b79874720de282e6d459b02
Jarrod Swords <jarrod.swords@gmail.com>
2022-07-07 11:55:38 -0400

    add customer infra

---
+++ customer-infrastructure/source/Context.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static System.Guid;
+
+namespace BoundedContextDemo.Infrastructure.Customers;
+
+public class Context : DbContext
+{
+    #region Public Interface
+
+    public DbSet<Customer> Customer { get; set; }
+
+    #endregion
+
+    #region Protected Interface
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Data Source=BECKY\\SQLEXPRESS;Initial Catalog=CustomerData;Integrated Security=True;Connect Timeout=60;"
+        );
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>().HasData(
+            new Customer { Id = NewGuid(), Name = "John", Surname = "Doe" },
+            new Customer { Id = NewGuid(), Name = "Jane", Surname = "Doe" }
+        );
+    }
+
+    #endregion
+}
---
+++ customer-infrastructure/source/_entities/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace BoundedContextDemo.Infrastructure.Customers;
+
+public class Customer : Entity
+{
+    #region Public Interface
+
+    public string Name { get; set; }
+    public string Surname { get; set; }
+
+    #endregion
+}
