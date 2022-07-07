02c6b8cfbb46484dd5d7599d66aa9d979161dfe9
Jarrod Swords <jarrod.swords@gmail.com>
2022-07-07 12:51:04 -0400

    add view

---
+++ infrastructure/Migrations/20220707155915_AddCustomerView.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BoundedContextDemo.Infrastructure.Migrations
+{
+    public partial class AddCustomerView : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- infrastructure/source/Context.cs
+++ infrastructure/source/Context.cs
@@ -6,6 +6,7 @@ public class Context : DbContext
 {
     #region Public Interface
 
+    public DbSet<Customer> Customer { get; set; }
     public DbSet<Product> Product { get; set; }
 
     #endregion
@@ -17,6 +18,11 @@ public class Context : DbContext
         optionsBuilder.UseSqlServer(
             "Data Source=BECKY\\SQLEXPRESS;Initial Catalog=BoundedContextDemo;Integrated Security=True;Connect Timeout=60;"
         );
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>(x => x.ToView("CustomerView"));
     }
 
     #endregion
